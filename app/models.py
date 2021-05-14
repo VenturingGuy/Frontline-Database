f063561240b7bbd7193939e35c9ca746b3c09d5d
Venturing Guy <LVentura335@Gmail.com>
2021-05-14 08:22:31 -0700

    removed model table that wasn't being used

--- app/models.py
+++ app/models.py
@@ -40,13 +40,7 @@ class MechAttack(db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
         return f"<User: {self.username}>"
-
-mech_attack_table = db.Table(
-    "mech_attack_table",
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
-    db.Column("mech_id", db.Integer, db.ForeignKey("mech.id")),
-)
