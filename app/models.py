9d22415f3075b9fbec9744ffc2e54babfd884641
Venturing Guy <LVentura335@Gmail.com>
2021-05-14 08:06:08 -0700

    fixed password error

--- app/models.py
+++ app/models.py
@@ -40,7 +40,7 @@ class MechAttack(db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
         return f"<User: {self.username}>"
